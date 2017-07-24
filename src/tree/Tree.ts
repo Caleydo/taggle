f14a1846c9d24fab1463b87021dffbec1ba1010d
Martin Ennemoser <martin.ennemoser@jku.at>
2017-07-24 16:33:38 +0200

    Tree traversal implemented

--- src/tree/Tree.ts
+++ src/tree/Tree.ts
@@ -2,18 +2,26 @@
  * Created by Martin on 24.07.2017.
  */
 import {ITreeObserver, TreeEvent} from '../model/TreeModel';
-import {InnerNode} from '../tree';
+import {InnerNode, LeafNode} from '../tree';
 import TreeModel from '../model/TreeModel';
+import {visit} from '../tree';
+import {IRow} from '../data';
 
 export default class Tree implements ITreeObserver {
   private root: InnerNode;
   update(e: TreeEvent): void {
-    console.log(e);
+    if(e.sender === this) {
+      return;
+    }
   }
 
   public initFromExistingTree(root: InnerNode, treeModel: TreeModel) {
     treeModel.nodesAdded(null, [root], this);
     this.root = root;
+    visit<IRow>(root, (inner: InnerNode) => {
+      treeModel.nodesAdded(inner.parent, [inner], this);
+      return true;
+    }, (leaf: LeafNode<IRow>)=> treeModel.nodesAdded(leaf.parent, [leaf], this));
   }
 
   public get Root() {
