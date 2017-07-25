50a7603aa3a90a5f7b24dbeb600708b8386224f7
Martin Ennemoser <martin.ennemoser@jku.at>
2017-07-25 13:08:13 +0200

    Removed unused methods

--- src/treevis/CollapsibleList.ts
+++ src/treevis/CollapsibleList.ts
@@ -13,54 +13,6 @@ export default class CollapsibleList implements ITreeObserver {
     this.$node = d3.select(root).append('div').classed('treevis', true);
   }
 
-  render(root: InnerNode) {
-    const chooseItemData = (node: INode) => {
-      if(node.type === 'inner') {
-        // separate leafs and inner nodes
-        // if leaf count is > max leaf count then we just want to show a single node
-        const leaves = node.children.filter((x) => x.type === 'leaf');
-        const inners = node.children.filter((x) => x.type === 'inner');
-
-        if(leaves.length > this.maxLeafVisCount) {
-          inners.unshift(new LeafNode(leaves.length + ' items'));
-          return inners;
-        }
-
-        return node.children;
-      } else {
-        return [];
-      }
-    };
-
-    const renderLevel = ($node: d3.Selection<INode>, node: INode) => {
-      $node.classed(node.type, true);
-      const $li = $node.append('ul').classed('hidden', true).selectAll('li').data(chooseItemData(node));
-      $li.enter().append('li')
-        .on('click', function(this: HTMLElement) {
-          const $this = d3.select(this);
-          const hiddenVal = $this.select('ul').classed('hidden');
-          $this.select('ul').classed('hidden', !hiddenVal);
-          (<MouseEvent>d3.event).stopPropagation();
-        });
-      $li.text((d) => d.type === 'inner' ? this.buildInnerNodeLabel(d) : this.buildLeafNodeLabel(d));
-      $li.each(function(this: HTMLElement, d: InnerNode) {
-        renderLevel(d3.select(this), d);
-      });
-      $li.exit().remove();
-    };
-
-    // create new dummy root
-    console.assert(root.parent === null);
-    root.parent = new InnerNode('dummy node');
-    root.parent.children.push(root);
-    root = root.parent;
-    renderLevel(this.$node, root);
-    // remove dummy root again after rendering
-    root = <InnerNode> root.children[0];
-    root.parent = null;
-    this.$node.select('ul').classed('hidden', false);
-  }
-
   buildLeafNodeLabel(node: LeafNode<any>) {
     return node.item +
         ' (Current Height: ' + node.height +
@@ -103,7 +55,7 @@ export default class CollapsibleList implements ITreeObserver {
     });
   }
 
-  installNode($li: d3.Selection<any>, leaf: INode) {
+  private installNode($li: d3.Selection<any>, leaf: INode) {
      $li.on('click', function(this: HTMLElement) {
           const $this = d3.select(this);
           const hiddenVal = $this.select('ul').classed('hidden');
