39a62aea1271df82696d2c2118c88162205f750a
Martin Ennemoser <martin.ennemoser@jku.at>
2017-07-27 16:18:19 +0200

    Added collapsed state

--- src/treevis/CollapsibleList.ts
+++ src/treevis/CollapsibleList.ts
@@ -6,30 +6,33 @@ import {InnerNode, INode, visit} from '../tree';
 import {IRow} from '../data';
 
 export default class CollapsibleList {
-  private readonly $node: d3.Selection<any>;
+  private readonly $table: d3.Selection<any>;
 
   constructor(root: HTMLElement) {
-    this.$node = d3.select(root).append('div').classed('treevis', true);
+    this.$table = d3.select(root).append('div').classed('treevis', true).append('table');
   }
 
   render(root: InnerNode) {
-    const switchState = (val: string, selection: d3.Selection<any>) => {
-      const hiddenVal = selection.classed(val);
-      selection.classed(val, !hiddenVal);
+    const invertCollapsedState = (node: d3.Selection<any>) => {
+      const state = node.classed('collapsed');
+      node.classed('collapsed', !state);
     };
 
-    const buildTable = ($node: d3.Selection<INode>, arr: INode[], treeColumnCount: number) => {
-      console.assert($node && arr && treeColumnCount > -1);
-      const $table = $node.append('table');
-      const $rows = $table.selectAll('tr')
+    const buildTable = ($table: d3.Selection<INode>, arr: INode[], treeColumnCount: number) => {
+      console.assert($table && arr && treeColumnCount > -1);
+
+      $table
+        .append('thead')
+        .append('tr')
+        .append('th')
+        .attr('colspan', treeColumnCount).text('Visual Tree');
+
+      const $rows = $table.append('tbody').selectAll('tr')
           .data(arr);
-
-      $table.append('tr').
-      append('th').attr('colspan', treeColumnCount).text('Visual Tree');
-
       const $tr = $rows.enter().append('tr')
+        .classed('collapsed', false)
         .html((d) => {
-          return `${this.buildRow(treeColumnCount, d)}`;
+          return this.buildRow(treeColumnCount, d);
         });
 
       // collapses all nodes that are on a deeper level
@@ -37,8 +40,9 @@ export default class CollapsibleList {
         if(!this.parentNode) { //should never happen
           return;
         }
-        const $this = d3.select(this.parentNode);
-        const thisLevel = $this.datum().level;
+        const $firstItem = d3.select(this.parentNode);
+        const thisLevel = $firstItem.datum().level;
+        invertCollapsedState($firstItem);
 
         // visit all siblings
         // hide all siblings until a sibling was found that is on the same level
@@ -48,8 +52,10 @@ export default class CollapsibleList {
         }
         let $sibling = d3.select(sibling);
         let nextLevel = $sibling.datum().level;
+        const collapse = $firstItem.classed('collapsed');
         while(thisLevel !== nextLevel) {
-          switchState('hidden', $sibling);
+          $sibling.classed('hidden', collapse);
+          $sibling.classed('collapsed', false);
           sibling = sibling.nextSibling;
           if(!sibling) {
             break;
@@ -61,12 +67,11 @@ export default class CollapsibleList {
       $rows.exit().remove();
     };
 
-    // create new dummy root
     console.assert(root.parent === null);
 
     const arr: INode[] = [];
     const treeDepth = this.flat(root, arr); // convert tree to list
-    buildTable(this.$node, arr, treeDepth+1);
+    buildTable(this.$table, arr, treeDepth+1);
   }
 
   private buildRow(treeColumnCount: number, node: INode) {
