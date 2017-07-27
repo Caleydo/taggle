7299b3742001289cc251e352b0d588fa12912a47
Martin Ennemoser <martin.ennemoser@jku.at>
2017-07-27 14:14:36 +0200

    Implemented tree vis as table

--- src/treevis/CollapsibleList.ts
+++ src/treevis/CollapsibleList.ts
@@ -2,67 +2,99 @@
  * Created by Martin on 19.07.2017.
  */
 import * as d3 from 'd3';
-import {InnerNode, INode, LeafNode} from '../tree';
+import {InnerNode, INode, visit} from '../tree';
+import {IRow} from '../data';
 
 export default class CollapsibleList {
   private readonly $node: d3.Selection<any>;
 
-  constructor(root: HTMLElement, private readonly maxLeafVisCount = 20) {
+  constructor(root: HTMLElement, private readonly maxLeafVisCount = 5) {
     this.$node = d3.select(root).append('div').classed('treevis', true);
   }
 
   render(root: InnerNode) {
-    const chooseItemData = (node: INode) => {
-      if(node.type === 'inner') {
-
-        // separate leafs and inner nodes
-        // if leaf count is > max leaf count then we just want to show a single node
-        const numLeaves = node.children.reduce((a, n) => a + (n.type === 'leaf' ? 1 : 0), 0);
-        const inners = node.children.filter((x) => x.type === 'inner');
-
-        if(numLeaves > this.maxLeafVisCount) {
-          inners.unshift(new LeafNode('${numLeaves} items'));
-          return inners;
-        }
-
-        return node.children;
-      }
-      return [];
+    const switchState = (val: string, selection: d3.Selection<any>) => {
+      const hiddenVal = selection.classed(val);
+      selection.classed(val, !hiddenVal);
     };
 
-    const renderLevel = ($node: d3.Selection<INode>, node: INode) => {
-      $node.classed(node.type, true);
-      const $li = $node.append('ul').classed('hidden', true).selectAll('li').data(chooseItemData(node));
-      $li.enter().append('li')
-        .on('click', function(this: HTMLElement) {
-          const $this = d3.select(this);
-          const hiddenVal = $this.select('ul').classed('hidden');
-          $this.select('ul').classed('hidden', !hiddenVal);
-          (<MouseEvent>d3.event).stopPropagation();
+    const buildTable = ($node: d3.Selection<INode>, arr: INode[], treeColumnCount: number) => {
+      const $table = $node.append('table');
+      console.assert($node && arr && treeColumnCount > -1);
+      console.log(this.maxLeafVisCount);
+      const $rows = $table.selectAll('tr')
+          .data(arr);
+
+      $table.append('tr').
+      append('th').attr('colspan', treeColumnCount).text('Visual Tree');
+
+      const $tr = $rows.enter().append('tr')
+        .html((d) => {
+          return `${this.buildRow(treeColumnCount, d)}`;
         });
-      $li.text((d) => d.type === 'inner' ? this.buildInnerNodeLabel(d) : this.buildLeafNodeLabel(d));
-      $li.each(function(this: HTMLElement, d: InnerNode) {
-        renderLevel(d3.select(this), d);
+
+      // collapses all nodes that are on a deeper level
+      $tr.select('.clickable').on('click', function(this: HTMLElement) {
+        if(!this.parentNode) { //should never happen
+          return;
+        }
+        const $this = d3.select(this.parentNode);
+        const thisLevel = $this.datum().level;
+
+        // visit all siblings
+        // hide all siblings until a sibling was found that is on the same level
+        let sibling = this.parentNode.nextSibling;
+        if(!sibling) {
+          return;
+        }
+        let $sibling = d3.select(sibling);
+        let nextLevel = $sibling.datum().level;
+        while(thisLevel !== nextLevel) {
+          switchState('hidden', $sibling);
+          sibling = sibling.nextSibling;
+          if(!sibling) {
+            break;
+          }
+          $sibling = d3.select(sibling);
+          nextLevel = $sibling.datum().level;
+        }
       });
-      $li.exit().remove();
+      $rows.exit().remove();
     };
 
     // create new dummy root
     console.assert(root.parent === null);
 
-    const newRoot = new InnerNode('dummy node');
-    newRoot.children.push(root);
-    renderLevel(this.$node, newRoot);
-    this.$node.select('ul').classed('hidden', false);
+    const arr: INode[] = [];
+    const treeDepth = this.flat(root, arr); // convert tree to list
+    buildTable(this.$node, arr, treeDepth+1);
   }
 
-  private buildLeafNodeLabel(node: LeafNode<any>) {
-    return `${node.item} (Current Height: ${node.height} | Renderer: ${node.renderer} )`;
+  private buildRow(treeColumnCount: number, node: INode) {
+    let htmlString = '';
+    for(let i = 0; i < treeColumnCount; i++) {
+      if(i === node.level) {
+        const text = node.level === 0 ? 'root' : node.toString();
+         htmlString = htmlString.concat(`<td class="clickable">${text}</td>`);
+         continue;
+      }
+      htmlString = htmlString.concat(`<td></td>`);
+    }
+    return htmlString;
   }
 
-  private buildInnerNodeLabel(node: InnerNode) {
-    return `${node.name} (Child Count: ${node.children.length} | Current Height: ${node.height} | Aggr. Height: ${node.aggregatedHeight} | Renderer: ${node.renderer} )`;
+  private flat(root: INode, arr: INode[]) {
+    console.assert(root);
+    let depth = 0;
+    visit<IRow>(root, (inner: InnerNode) => {
+      arr.push(inner);
+      return true;
+    }, (n) => {
+      arr.push(n);
+      if(n.level > depth) {
+        depth = n.level;
+      }
+    });
+    return depth;
   }
 }
-
-
