38ede8e7944f56d1a534cccd6cd6d527e1d87bae
Martin Ennemoser <martin.ennemoser@jku.at>
2017-08-02 10:28:12 +0200

    Added font-awesome as dependency

--- src/controls/FlyoutBar.ts
+++ src/controls/FlyoutBar.ts
@@ -8,7 +8,7 @@ export default class FlyoutBar {
     this.$div = d3.select(root).append('div').classed('flyout', true).on('click', this.handleClick).append('h1').text('Configure Tree');
   }
 
-  handleClick() {
+  private handleClick() {
     d3.select('div.treevis').style('display', 'block');
     d3.select('div.flyout').style('display', 'none');
   }
