21495a24b6be7edff81cc5b49d140baa5535077d
Holger Stitz <holger.stitz@jku.at>
2017-09-20 18:49:03 +0200

    Removed proportional flag entirely
    sgratzl/taggle2#99

--- src/rule/RuleButtonSwitcher.ts
+++ src/rule/RuleButtonSwitcher.ts
@@ -8,46 +8,32 @@ import {notSpacefillingNotProportional, spacefillingNotProportional, notSpacefil
 export default class RuleButtonSwitcher {
   readonly node = document.createElement('div');
 
-  private readonly proportional: HTMLElement;
   private readonly spaceFilling: HTMLElement;
 
   constructor(private readonly callback: (ruleSet: IRuleSetLike)=>void) {
 
     this.node.classList.add('rule-button-chooser');
     this.node.innerHTML = `
-      <!--<div><span>Proportional</span>
-        <code></code>
-      </div>-->
       <div><span>Overview</span>
         <code></code>
       </div>
     `;
 
-    this.proportional = <HTMLElement>this.node.firstElementChild!;
-    this.spaceFilling = <HTMLElement>this.node.lastElementChild!;
+    this.spaceFilling = <HTMLElement>this.node.firstElementChild!;
 
     const update = (evt: MouseEvent) => {
       (<HTMLElement>evt.currentTarget).classList.toggle('chosen');
       this.callback(this.choose());
     };
-    this.proportional.addEventListener('click', update);
     this.spaceFilling.addEventListener('click', update);
   }
 
   setViolations(violations: IRuleViolations) {
-    this.proportional.classList.toggle('violated', Boolean(violations.proportionalRatios));
-    this.proportional.lastElementChild!.textContent = (violations.proportionalRatios || '').replace(/\n/g,'<br>');
-
     this.spaceFilling.classList.toggle('violated', Boolean(violations.spaceFilling));
     this.spaceFilling.lastElementChild!.textContent = (violations.spaceFilling || '').replace(/\n/g,'<br>');
   }
 
   private choose() {
-    const spaceFilling = this.spaceFilling.classList.contains('chosen');
-    const proportional = this.proportional.classList.contains('chosen');
-    if (spaceFilling) {
-      return proportional ? spacefillingProportional : spacefillingNotProportional;
-    }
-    return proportional ? notSpacefillingProportional: notSpacefillingNotProportional;
+    return this.spaceFilling.classList.contains('chosen') ? spacefillingNotProportional : notSpacefillingNotProportional;
   }
 }
